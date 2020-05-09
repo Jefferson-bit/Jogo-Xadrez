b065790974c6d652816330e02223b5aa22113ff5
jefferson-bit <jhonsonbabi@hotmail.com>
2020-05-09 13:11:49 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChechException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChechException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChechException( String msg) {
+		super(msg);
+	}
+}
