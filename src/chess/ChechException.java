2853eba89a0b76e52c3f8183580ea762d06899d9
jefferson-bit <jhonsonbabi@hotmail.com>
2020-05-09 14:36:17 -0300

    Moving pieces

--- src/chess/ChechException.java
+++ src/chess/ChechException.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChechException extends RuntimeException {
+import boardgame.BoardException;
+
+public class ChechException extends BoardException {
 
 	private static final long serialVersionUID = 1L;
 	
